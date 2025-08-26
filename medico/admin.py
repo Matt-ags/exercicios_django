631cfcaeae25f9f4e50355397c107cf167b5d788
Matt-ags <mateusabnelocca@gmail.com>
2025-08-26 15:43:27 -0300

    Add medico app with models, migrations, and admin registration

--- medico/admin.py
+++ medico/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ESPECIALIDADE, MEDICO
+admin.site.register(ESPECIALIDADE)
+admin.site.register(MEDICO)
--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class ESPECIALIDADE(models.Model):
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id_especialidade = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField() 
+
+class MEDICO(models.Model):
+    id_medico = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    data_nascimento = models.DateField()
+    crm = models.CharField(max_length=20)
+    id_especialidade = models.ForeignKey(ESPECIALIDADE, on_delete=models.CASCADE)
+
